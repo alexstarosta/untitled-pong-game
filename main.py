89790ce5ba0ddf7920524ec97f409fa2e6d1abbc
alexstarosta <alexstarostadev@gmail.com>
2022-12-30 23:30:01 -0500

    added game icon

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from settings import *
 from elements import *
 from menu import *
 
+programIcon = pygame.image.load('assets/icon.png')
+pygame.display.set_icon(programIcon)
+
 menu = Menu(SFX, PARTICLES, GAMEMODE, "normal")
 menu.setup()
 menu.run()
